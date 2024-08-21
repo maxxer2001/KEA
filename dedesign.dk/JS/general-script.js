99a0eb87880cd62a1093d5b44308b61ea0405345
Massimo Dede <112664143+maxxer2001@users.noreply.github.com>
2024-08-21 19:30:28 +0200

    Add files via upload

---
+++ dedesign.dk/JS/general-script.js
@@ -0,0 +1,11 @@
+// general-script.js
+
+// Burger Menu
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    burger.classList.toggle('active');
+});
+
